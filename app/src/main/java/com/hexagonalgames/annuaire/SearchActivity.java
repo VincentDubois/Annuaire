ce5428f09e30d2fc4104369d33eac728e5e8b18a
Marianne <marianne.c62@gmail.com>
2017-03-08 10:42:50 +0100

    -> ajout de la search activity (pour l'inclure dans les activités concernées) -> placement des éléments -> include searchbar dans main

--- app/src/main/java/com/hexagonalgames/annuaire/SearchActivity.java
+++ app/src/main/java/com/hexagonalgames/annuaire/SearchActivity.java
@@ -1,13 +1,42 @@
 package com.hexagonalgames.annuaire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements TextView.OnEditorActionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        EditText searchView = (EditText) findViewById(R.id.search);
+        searchView.setOnEditorActionListener(this);
+/*
+
+*/
+    }
+
+    @Override
+    public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
+        if ( actionId == EditorInfo.IME_ACTION_DONE ||
+                ( event != null && event.getAction() == KeyEvent.ACTION_DOWN &&
+                        event.getKeyCode() == KeyEvent.KEYCODE_ENTER) ) {
+
+            // Traitement de l'évènement
+            EditText txtname = (EditText)findViewById(R.id.search);
+            String search      =  txtname.getText().toString();
+
+            Intent intent = new Intent(this, PersonneActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+        return false;
     }
 }
